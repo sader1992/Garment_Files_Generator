2790711dc1ab53f2a8cff8457001dc3c83e8fb92
sader fawall <sader1992@hotmail.com>
2017-07-27 15:51:12 +0200

    Add project files.

---
+++ Garment Files Generator/Form1.Designer.cs
@@ -0,0 +1,340 @@
+﻿namespace Garment_Files_Generator
+{
+    partial class GFG
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GFG));
+            this.textBoxSPR = new System.Windows.Forms.TextBox();
+            this.textBoxACT = new System.Windows.Forms.TextBox();
+            this.buttonACT = new System.Windows.Forms.Button();
+            this.buttonSPR = new System.Windows.Forms.Button();
+            this.ButtonGENERATE = new System.Windows.Forms.Button();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxSPRd = new System.Windows.Forms.TextBox();
+            this.textBoxACTd = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxitm = new System.Windows.Forms.TextBox();
+            this.textBoxcoll = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.buttonSPRd = new System.Windows.Forms.Button();
+            this.buttonACTd = new System.Windows.Forms.Button();
+            this.buttonitm = new System.Windows.Forms.Button();
+            this.buttoncoll = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBoxinfo = new System.Windows.Forms.TextBox();
+            this.textBoxGName = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBoxSPR
+            // 
+            this.textBoxSPR.Location = new System.Drawing.Point(24, 86);
+            this.textBoxSPR.Name = "textBoxSPR";
+            this.textBoxSPR.ReadOnly = true;
+            this.textBoxSPR.Size = new System.Drawing.Size(294, 20);
+            this.textBoxSPR.TabIndex = 1;
+            // 
+            // textBoxACT
+            // 
+            this.textBoxACT.Location = new System.Drawing.Point(24, 133);
+            this.textBoxACT.Name = "textBoxACT";
+            this.textBoxACT.ReadOnly = true;
+            this.textBoxACT.Size = new System.Drawing.Size(294, 20);
+            this.textBoxACT.TabIndex = 2;
+            // 
+            // buttonACT
+            // 
+            this.buttonACT.Location = new System.Drawing.Point(324, 131);
+            this.buttonACT.Name = "buttonACT";
+            this.buttonACT.Size = new System.Drawing.Size(125, 23);
+            this.buttonACT.TabIndex = 3;
+            this.buttonACT.Text = "Browse - ACT";
+            this.buttonACT.UseVisualStyleBackColor = true;
+            this.buttonACT.Click += new System.EventHandler(this.buttonACT_Click);
+            // 
+            // buttonSPR
+            // 
+            this.buttonSPR.Location = new System.Drawing.Point(324, 84);
+            this.buttonSPR.Name = "buttonSPR";
+            this.buttonSPR.Size = new System.Drawing.Size(125, 23);
+            this.buttonSPR.TabIndex = 4;
+            this.buttonSPR.Text = "Browse - SPR";
+            this.buttonSPR.UseVisualStyleBackColor = true;
+            this.buttonSPR.Click += new System.EventHandler(this.buttonSPR_Click);
+            // 
+            // ButtonGENERATE
+            // 
+            this.ButtonGENERATE.Location = new System.Drawing.Point(24, 263);
+            this.ButtonGENERATE.Name = "ButtonGENERATE";
+            this.ButtonGENERATE.Size = new System.Drawing.Size(425, 55);
+            this.ButtonGENERATE.TabIndex = 5;
+            this.ButtonGENERATE.Text = "GENERATE";
+            this.ButtonGENERATE.UseVisualStyleBackColor = true;
+            this.ButtonGENERATE.Click += new System.EventHandler(this.ButtonGENERATE_Click);
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(32, 36);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(132, 20);
+            this.textBoxName.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(151, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Garment File Name (no space)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Spirt File";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(29, 117);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Act File";
+            // 
+            // textBoxSPRd
+            // 
+            this.textBoxSPRd.Location = new System.Drawing.Point(24, 182);
+            this.textBoxSPRd.Name = "textBoxSPRd";
+            this.textBoxSPRd.ReadOnly = true;
+            this.textBoxSPRd.Size = new System.Drawing.Size(110, 20);
+            this.textBoxSPRd.TabIndex = 8;
+            this.textBoxSPRd.TextChanged += new System.EventHandler(this.textBoxSPRd_TextChanged);
+            // 
+            // textBoxACTd
+            // 
+            this.textBoxACTd.Location = new System.Drawing.Point(24, 227);
+            this.textBoxACTd.Name = "textBoxACTd";
+            this.textBoxACTd.ReadOnly = true;
+            this.textBoxACTd.Size = new System.Drawing.Size(110, 20);
+            this.textBoxACTd.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(24, 166);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(73, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Spirt drop File";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(24, 211);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(67, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Act drop File";
+            // 
+            // textBoxitm
+            // 
+            this.textBoxitm.Location = new System.Drawing.Point(258, 182);
+            this.textBoxitm.Name = "textBoxitm";
+            this.textBoxitm.ReadOnly = true;
+            this.textBoxitm.Size = new System.Drawing.Size(110, 20);
+            this.textBoxitm.TabIndex = 12;
+            // 
+            // textBoxcoll
+            // 
+            this.textBoxcoll.Location = new System.Drawing.Point(258, 227);
+            this.textBoxcoll.Name = "textBoxcoll";
+            this.textBoxcoll.ReadOnly = true;
+            this.textBoxcoll.Size = new System.Drawing.Size(110, 20);
+            this.textBoxcoll.TabIndex = 13;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(255, 166);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(53, 13);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Item Icon";
+            // 
+            // buttonSPRd
+            // 
+            this.buttonSPRd.Location = new System.Drawing.Point(140, 180);
+            this.buttonSPRd.Name = "buttonSPRd";
+            this.buttonSPRd.Size = new System.Drawing.Size(75, 23);
+            this.buttonSPRd.TabIndex = 15;
+            this.buttonSPRd.Text = "SPR";
+            this.buttonSPRd.UseVisualStyleBackColor = true;
+            this.buttonSPRd.Click += new System.EventHandler(this.buttonSPRd_Click);
+            // 
+            // buttonACTd
+            // 
+            this.buttonACTd.Location = new System.Drawing.Point(140, 224);
+            this.buttonACTd.Name = "buttonACTd";
+            this.buttonACTd.Size = new System.Drawing.Size(75, 23);
+            this.buttonACTd.TabIndex = 16;
+            this.buttonACTd.Text = "ACT";
+            this.buttonACTd.UseVisualStyleBackColor = true;
+            this.buttonACTd.Click += new System.EventHandler(this.buttonACTd_Click);
+            // 
+            // buttonitm
+            // 
+            this.buttonitm.Location = new System.Drawing.Point(374, 180);
+            this.buttonitm.Name = "buttonitm";
+            this.buttonitm.Size = new System.Drawing.Size(75, 23);
+            this.buttonitm.TabIndex = 17;
+            this.buttonitm.Text = "SMALL BMP";
+            this.buttonitm.UseVisualStyleBackColor = true;
+            this.buttonitm.Click += new System.EventHandler(this.buttonitm_Click);
+            // 
+            // buttoncoll
+            // 
+            this.buttoncoll.Location = new System.Drawing.Point(374, 224);
+            this.buttoncoll.Name = "buttoncoll";
+            this.buttoncoll.Size = new System.Drawing.Size(75, 23);
+            this.buttoncoll.TabIndex = 18;
+            this.buttoncoll.Text = "LARGE BMP";
+            this.buttoncoll.UseVisualStyleBackColor = true;
+            this.buttoncoll.Click += new System.EventHandler(this.buttoncoll_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(255, 211);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(77, 13);
+            this.label7.TabIndex = 19;
+            this.label7.Text = "Collection Icon";
+            // 
+            // textBoxinfo
+            // 
+            this.textBoxinfo.Location = new System.Drawing.Point(455, 20);
+            this.textBoxinfo.Multiline = true;
+            this.textBoxinfo.Name = "textBoxinfo";
+            this.textBoxinfo.ReadOnly = true;
+            this.textBoxinfo.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxinfo.Size = new System.Drawing.Size(407, 298);
+            this.textBoxinfo.TabIndex = 20;
+            // 
+            // textBoxGName
+            // 
+            this.textBoxGName.Location = new System.Drawing.Point(258, 36);
+            this.textBoxGName.Name = "textBoxGName";
+            this.textBoxGName.Size = new System.Drawing.Size(132, 20);
+            this.textBoxGName.TabIndex = 6;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(266, 20);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(118, 13);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Garment InGame Name";
+            // 
+            // GFG
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(876, 329);
+            this.Controls.Add(this.textBoxinfo);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.buttoncoll);
+            this.Controls.Add(this.buttonitm);
+            this.Controls.Add(this.buttonACTd);
+            this.Controls.Add(this.buttonSPRd);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBoxcoll);
+            this.Controls.Add(this.textBoxitm);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxACTd);
+            this.Controls.Add(this.textBoxSPRd);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxGName);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.ButtonGENERATE);
+            this.Controls.Add(this.buttonSPR);
+            this.Controls.Add(this.buttonACT);
+            this.Controls.Add(this.textBoxACT);
+            this.Controls.Add(this.textBoxSPR);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.ImeMode = System.Windows.Forms.ImeMode.Disable;
+            this.Name = "GFG";
+            this.Text = "Garment Files Generator v1.0                                                     " +
+    "                  By Sader Fawall (sader1992)";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBoxSPR;
+        private System.Windows.Forms.TextBox textBoxACT;
+        private System.Windows.Forms.Button buttonACT;
+        private System.Windows.Forms.Button buttonSPR;
+        private System.Windows.Forms.Button ButtonGENERATE;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxSPRd;
+        private System.Windows.Forms.TextBox textBoxACTd;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxitm;
+        private System.Windows.Forms.TextBox textBoxcoll;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button buttonSPRd;
+        private System.Windows.Forms.Button buttonACTd;
+        private System.Windows.Forms.Button buttonitm;
+        private System.Windows.Forms.Button buttoncoll;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBoxinfo;
+        private System.Windows.Forms.TextBox textBoxGName;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ Garment Files Generator/Form1.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Garment_Files_Generator
+{
+    public partial class GFG : Form
+    {   
+        public GFG()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonSPR_Click(object sender, EventArgs e)
+        {
+            var SPRF = new System.Windows.Forms.OpenFileDialog();
+            SPRF.Filter = "SPR Files|*.spr";
+            if (SPRF.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                string fileToOpen = SPRF.FileName;
+                System.IO.FileInfo File = new System.IO.FileInfo(SPRF.FileName);
+                System.IO.StreamReader reader = new System.IO.StreamReader(fileToOpen);
+                textBoxSPR.Text = SPRF.FileName;
+            }
+        }
+
+        private void buttonACT_Click(object sender, EventArgs e)
+        {
+            var ACTF = new System.Windows.Forms.OpenFileDialog();
+            ACTF.Filter = "ACT Files|*.act";
+            if (ACTF.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                string fileToOpen = ACTF.FileName;
+                System.IO.FileInfo File = new System.IO.FileInfo(ACTF.FileName);
+                System.IO.StreamReader reader = new System.IO.StreamReader(fileToOpen);
+                textBoxACT.Text = ACTF.FileName;
+            }
+        }
+        private void ButtonGENERATE_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(textBoxName.Text))
+            {
+                MessageBox.Show("Input Garment file name.");
+                return;
+            }
+            if (string.IsNullOrWhiteSpace(textBoxGName.Text))
+            {
+                MessageBox.Show("Input Garment name.");
+                return;
+            }
+            if (string.IsNullOrWhiteSpace(textBoxSPR.Text))
+            {
+                MessageBox.Show("Import the main Spirt file");
+                return;
+            }
+            if (string.IsNullOrWhiteSpace(textBoxACT.Text))
+            {
+                MessageBox.Show("Import the main Act file");
+                return;
+            }
+            if (string.IsNullOrWhiteSpace(textBoxSPRd.Text))
+            {
+                MessageBox.Show("Import the Drop Spirt file");
+                return;
+            }
+            if (string.IsNullOrWhiteSpace(textBoxACTd.Text))
+            {
+                MessageBox.Show("Import the Drop Act file");
+                return;
+            }
+            if (string.IsNullOrWhiteSpace(textBoxitm.Text))
+            {
+                MessageBox.Show("Import the Item BMP file");
+                return;
+            }
+            if (string.IsNullOrWhiteSpace(textBoxcoll.Text))
+            {
+                MessageBox.Show("Import the Collection BMP file");
+                return;
+            }
+            string path = Path.GetDirectoryName(Application.ExecutablePath);
+            var G_FILENAME = textBoxName.Text;
+            var G_INGAME = textBoxGName.Text;
+            var SPRFR = textBoxSPR.Text;
+            var ACTFR = textBoxACT.Text;
+            var SPRFRd = textBoxSPRd.Text;
+            var ACTFRd = textBoxACTd.Text;
+            var itmbmp = textBoxitm.Text;
+            var collbmp = textBoxcoll.Text;
+            string pathdatam = path + @"\data\sprite\로브\" + G_FILENAME+ @"\남";
+            string pathdataf = path + @"\data\sprite\로브\" + G_FILENAME + @"\여";
+            string pathdatal = path + @"\data\sprite\아이템";
+            string pathdatai = path + @"\data\texture\유저인터페이스\item";
+            string pathdatac = path + @"\data\texture\유저인터페이스\collection";
+            if (!Directory.Exists(pathdatam)) {
+                DirectoryInfo DATAFm = Directory.CreateDirectory(pathdatam);
+            }
+            if (!Directory.Exists(pathdataf))
+            {
+                DirectoryInfo DATAFf = Directory.CreateDirectory(pathdataf);
+            }
+            if (!Directory.Exists(pathdatal))
+            {
+                DirectoryInfo DATAFl = Directory.CreateDirectory(pathdatal);
+            }
+            if (!Directory.Exists(pathdatai))
+            {
+                DirectoryInfo DATAFi = Directory.CreateDirectory(pathdatai);
+            }
+            if (!Directory.Exists(pathdatac))
+            {
+                DirectoryInfo DATAFc = Directory.CreateDirectory(pathdatac);
+            }
+            System.IO.File.Copy(SPRFRd, path + @"\data\sprite\아이템\" + G_FILENAME + ".spr" , true);
+            System.IO.File.Copy(ACTFRd, path + @"\data\sprite\아이템\" + G_FILENAME + ".act", true);
+            System.IO.File.Copy(itmbmp, path + @"\data\texture\유저인터페이스\item\" + G_FILENAME + ".bmp" , true);
+            System.IO.File.Copy(collbmp, path + @"\data\texture\유저인터페이스\collection\" + G_FILENAME + ".bmp", true);
+            for (int i = 0; i < KroNames.boy.Length; i++)
+            {
+                System.IO.File.Copy(SPRFR, path + @"\data\sprite\로브\" + G_FILENAME + @"\남\" + KroNames.boy[i] + ".spr", true);
+                System.IO.File.Copy(ACTFR, path + @"\data\sprite\로브\" + G_FILENAME + @"\남\" + KroNames.boy[i] + ".act", true);
+
+                System.IO.File.Copy(SPRFR, path + @"\data\sprite\로브\" + G_FILENAME + @"\여\" + KroNames.girl[i] + ".spr", true);
+                System.IO.File.Copy(ACTFR, path + @"\data\sprite\로브\" + G_FILENAME + @"\여\" + KroNames.girl[i] + ".act", true);
+            }
+            textBoxinfo.Text += "------------------------------------------------------" + Environment.NewLine;
+            textBoxinfo.Text += "==================================" + Environment.NewLine;
+            textBoxinfo.Text += "------------------------------------------------------" + Environment.NewLine;
+            textBoxinfo.Text += "iteminfo.lua/iteminfo.lub :" + Environment.NewLine;
+            textBoxinfo.Text += "[<ItemID>] = {" + Environment.NewLine;
+            textBoxinfo.Text += "   unidentifiedDisplayName = \"" + G_INGAME + "\"," + Environment.NewLine;
+            textBoxinfo.Text += "   unidentifiedResourceName = \"" + G_FILENAME + "\"," + Environment.NewLine;
+            textBoxinfo.Text += "   unidentifiedDescriptionName = {" + Environment.NewLine;
+            textBoxinfo.Text += "       \"Description\"," + Environment.NewLine;
+            textBoxinfo.Text += "       \"^ffffff_^000000\"," + Environment.NewLine;
+            textBoxinfo.Text += "       \"Weight: ^7777777^000000\"," + Environment.NewLine;
+            textBoxinfo.Text += "   }," + Environment.NewLine;
+            textBoxinfo.Text += "   identifiedDisplayName = = \"" + G_INGAME + "\"," + Environment.NewLine;
+            textBoxinfo.Text += "   identifiedResourceName = \"" + G_FILENAME + "\"," + Environment.NewLine;
+            textBoxinfo.Text += "   identifiedDescriptionName = {" + Environment.NewLine;
+            textBoxinfo.Text += "       \"Description\"," + Environment.NewLine;
+            textBoxinfo.Text += "       \"^ffffff_^000000\"," + Environment.NewLine;
+            textBoxinfo.Text += "       \"Weight: ^7777777^000000\"," + Environment.NewLine;
+            textBoxinfo.Text += "   }," + Environment.NewLine;
+            textBoxinfo.Text += "   slotCount = 0," + Environment.NewLine;
+            textBoxinfo.Text += "   ClassNum = <ViewID>" + Environment.NewLine;
+            textBoxinfo.Text += "}," + Environment.NewLine;
+            textBoxinfo.Text += "------------------------------------------------------" + Environment.NewLine;
+            textBoxinfo.Text += "spriterobename.lua/spriterobename.lub :" + Environment.NewLine;
+            textBoxinfo.Text += "RobeNameTable = {" + Environment.NewLine;
+            textBoxinfo.Text += "   [SPRITE_ROBE_IDs.ROBE_SwordWing] = \"" + G_FILENAME + "\"" + Environment.NewLine;
+            textBoxinfo.Text += "}" + Environment.NewLine;
+            textBoxinfo.Text += "RobeNameTable_Eng = {" + Environment.NewLine;
+            textBoxinfo.Text += "   [SPRITE_ROBE_IDs.ROBE_" + G_FILENAME + "] = \"" + G_FILENAME + "\"" + Environment.NewLine;
+            textBoxinfo.Text += "}" + Environment.NewLine;
+            textBoxinfo.Text += "------------------------------------------------------" + Environment.NewLine;
+            textBoxinfo.Text += "spriterobeid.lua/spriterobeid.lub :" + Environment.NewLine;
+            textBoxinfo.Text += "SPRITE_ROBE_IDs = {" + Environment.NewLine;
+            textBoxinfo.Text += "   ROBE_" + G_FILENAME + " = <ViewID>" + Environment.NewLine;
+            textBoxinfo.Text += "}" + Environment.NewLine;
+            textBoxinfo.Text += "------------------------------------------------------" + Environment.NewLine;
+            textBoxinfo.Text += " " + Environment.NewLine;
+            MessageBox.Show("Done");
+            
+        }
+
+        private void textBoxSPRd_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonSPRd_Click(object sender, EventArgs e)
+        {
+            var SPRFd = new System.Windows.Forms.OpenFileDialog();
+            SPRFd.Filter = "SPR Files|*.spr";
+            if (SPRFd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                string fileToOpen = SPRFd.FileName;
+                System.IO.FileInfo File = new System.IO.FileInfo(SPRFd.FileName);
+                System.IO.StreamReader reader = new System.IO.StreamReader(fileToOpen);
+                textBoxSPRd.Text = SPRFd.FileName;
+            }
+        }
+
+        private void buttonACTd_Click(object sender, EventArgs e)
+        {
+            var ACTFd = new System.Windows.Forms.OpenFileDialog();
+            ACTFd.Filter = "ACT Files|*.act";
+            if (ACTFd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                string fileToOpen = ACTFd.FileName;
+                System.IO.FileInfo File = new System.IO.FileInfo(ACTFd.FileName);
+                System.IO.StreamReader reader = new System.IO.StreamReader(fileToOpen);
+                textBoxACTd.Text = ACTFd.FileName;
+            }
+        }
+
+        private void buttonitm_Click(object sender, EventArgs e)
+        {
+            var itmbmp = new System.Windows.Forms.OpenFileDialog();
+            itmbmp.Filter = "BMP Files|*.bmp";
+            if (itmbmp.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                string fileToOpen = itmbmp.FileName;
+                System.IO.FileInfo File = new System.IO.FileInfo(itmbmp.FileName);
+                System.IO.StreamReader reader = new System.IO.StreamReader(fileToOpen);
+                textBoxitm.Text = itmbmp.FileName;
+            }
+        }
+
+        private void buttoncoll_Click(object sender, EventArgs e)
+        {
+            var collbmp = new System.Windows.Forms.OpenFileDialog();
+            collbmp.Filter = "BMP Files|*.bmp";
+            if (collbmp.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                string fileToOpen = collbmp.FileName;
+                System.IO.FileInfo File = new System.IO.FileInfo(collbmp.FileName);
+                System.IO.StreamReader reader = new System.IO.StreamReader(fileToOpen);
+                textBoxcoll.Text = collbmp.FileName;
+            }
+        }
+    }
+}
